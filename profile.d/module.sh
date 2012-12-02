bd330d63e14fbb7370eb473981eb3a06246a2f17
Daichi OBINATA <earth2001y@gmail.com>
2012-12-02 21:22:39 +0900

    module初期化スクリプトの条件式を修正

--- profile.d/module.sh
+++ profile.d/module.sh
@@ -3,7 +3,7 @@
 MODULE_HOME=/opt/Modules/3.2.9
 
 # module
-if [ -z "$MODULE_HOME" ]; then
+if [ -n "$MODULE_HOME" ]; then
 	source $MODULE_HOME/init/bash
 #	export PATH=$MODULE_HOME/bin:$PATH
 #	export MODULEPATH=$MODULE_HOME/modulefiles
