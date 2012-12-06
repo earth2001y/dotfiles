e5ac2166922b42a8516c386e287f734c0913a71f
だいち <daichi@daichi-no-MacBook-Air.local>
2012-12-06 20:37:27 +0900

    GCC4.7.2の環境用modulefileを追加

--- profile.d/module.sh
+++ profile.d/module.sh
@@ -4,6 +4,7 @@ MODULE_HOME=/opt/Modules/3.2.9
 
 # module
 if [ -n "$MODULE_HOME" ]; then
+	export MODULEPATH=$HOME/etc/modulefiles:$MODULE_HOME/modulefiles
 	source $MODULE_HOME/init/bash
 #	export PATH=$MODULE_HOME/bin:$PATH
 #	export MODULEPATH=$MODULE_HOME/modulefiles
