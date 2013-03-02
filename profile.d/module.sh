3a1d7a2aaf2faad3091f20223b669b3114ef1e84
Daichi OBINATA <earth2001y@gmail.com>
2013-03-02 22:34:33 +0900

    modulesの環境変数を変更

--- profile.d/module.sh
+++ profile.d/module.sh
@@ -4,10 +4,9 @@ MODULE_HOME=/opt/Modules/3.2.9
 
 # module
 if [ -n "$MODULE_HOME" ]; then
-	export MODULEPATH=$HOME/etc/modulefiles:$MODULE_HOME/modulefiles
-	source $MODULE_HOME/init/bash
-#	export PATH=$MODULE_HOME/bin:$PATH
-#	export MODULEPATH=$MODULE_HOME/modulefiles
- 	export MANPATH=$MODULE_HOME/share/man:$MANPATH
+# export MODULEPATH=$HOME/etc/modulefiles:$MODULE_HOME/modulefiles
+  export MODULEPATH=$HOME/etc/modulefiles
+  source $MODULE_HOME/init/bash
+  export MANPATH=$MODULE_HOME/share/man:$MANPATH
 fi
 
