e77cbead44ab796692d4e2205e22e091e5866a9a
Daichi OBINATA <earth2001y@gmail.com>
2013-03-21 11:29:15 +0900

    Moduleの初期化処理で、Moduleのインストールパスが有効かどうかチェックするコードを入れた

--- profile.d/module.sh
+++ profile.d/module.sh
@@ -3,10 +3,10 @@
 MODULE_HOME=/opt/Modules/3.2.9
 
 # module
-if [ -n "$MODULE_HOME" ]; then
+if [ -n "$MODULE_HOME" -a -d "$MODULE_HOME" ]; then
 # export MODULEPATH=$HOME/etc/modulefiles:$MODULE_HOME/modulefiles
   export MODULEPATH=$HOME/etc/modulefiles
-  source $MODULE_HOME/init/bash
   export MANPATH=$MODULE_HOME/share/man:$MANPATH
+  source $MODULE_HOME/init/bash
 fi
 
